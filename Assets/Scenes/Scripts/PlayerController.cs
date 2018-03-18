0c98f6cc698dc70f512174ef503a09f92664578f
emjalac <emjalac@gmail.com>
2018-03-18 18:36:31 -0500

    added single-player shooting

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -1,19 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
 
 	// Use this for initialization
-	// void Start () {
-	// 	
-	// }
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate (0, x, 0);
 		transform.Translate (0, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			Fire ();
+		}
+	}
+
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			             bulletPrefab,
+			             bulletSpawn.position,
+			             bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
 	}
 }
