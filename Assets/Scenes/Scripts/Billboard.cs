14b3009a0a6ac1768963e236df3ce26e5d2cca1e
emjalac <emjalac@gmail.com>
2018-03-23 00:59:55 -0500

    added health

---
+++ Assets/Scenes/Scripts/Billboard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (Camera.main.transform);
+	}
+}
---
+++ Assets/Scenes/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Destroy (gameObject);
+
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health> ();
+		if (health != null)
+		{
+			health.TakeDamage (10);
+		}
+	}
+}
