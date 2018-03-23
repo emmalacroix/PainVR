29acd211c4e089d77a71920909e3c45db70684c6
emjalac <emjalac@gmail.com>
2018-03-23 06:55:45 -0500

    updated spawn/respawn points

--- Assets/Scenes/Scripts/Health.cs
+++ Assets/Scenes/Scripts/Health.cs
@@ -10,6 +10,15 @@ public class Health : NetworkBehaviour {
 	public int currentHealth = maxHealth;
 	public RectTransform healthBar;
 	public bool destroyOnDeath;
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start ()
+	{
+		if (isLocalPlayer)
+		{
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+		}
+	}
 
 	public void TakeDamage(int amount)
 	{
@@ -46,8 +55,17 @@ public class Health : NetworkBehaviour {
 	{
 		if (isLocalPlayer)
 		{
-			//move back to zero location
-			transform.position = Vector3.zero;
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick a spawn point at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
 		}
 	}
 
