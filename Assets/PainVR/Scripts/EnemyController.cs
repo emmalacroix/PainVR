d5beab4633152b0b00826851de5c669948ec53fa
lacro058 <emjalac@gmail.com>
2018-04-08 18:23:50 -0500

    added basic enemy shooting ai

--- Assets/PainVR/Scripts/EnemyController.cs
+++ Assets/PainVR/Scripts/EnemyController.cs
@@ -10,6 +10,7 @@ public class EnemyController : NetworkBehaviour {
 	private int damage;
 	private Vector3 maxPosition;
 	private Vector3 minPosition;
+	private bool waiting;
 
 	// Use this for initialization
 	void Start () {
@@ -19,23 +20,51 @@ public class EnemyController : NetworkBehaviour {
 		var maxObj = GameObject.FindGameObjectWithTag ("MaxBoundary");
 		minPosition = minObj.transform.position;
 		maxPosition = maxObj.transform.position;
+		waiting = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Enemy AI movement goes here
-		var dir = transform.forward;
-		dir.Normalize ();
-		var z = Mathf.Pow(Mathf.Pow(dir.z, 2)+Mathf.Pow(dir.x, 2), 0.5f);
-		if (z < 0)
-			z = -z;
-		z = z * Time.deltaTime * speed;
-
-		transform.Translate (0, 0, z);
-		if (OutOfBounds())
+		
+		//Enemy AI shooting goes here
+		Transform target = null;
+		float dist = Mathf.Infinity;
+		PlayerController[] players = FindObjectsOfType<PlayerController> ();
+		foreach (PlayerController player in players)
 		{
-			transform.Translate (0, 0, -z);
-			transform.Rotate (0, 60, 0);
+			float distToPlayer = Vector3.Magnitude(player.transform.position - transform.position);
+			if (distToPlayer < 5.0f && distToPlayer < dist)
+			{
+				dist = distToPlayer;
+				target = player.transform;
+			}
+		}
+		if (target != null)
+		{
+			if (!waiting)
+			{
+				transform.LookAt (target);
+				CmdFire ();
+				waiting = true;
+				Invoke ("StopWaiting", 1.0f);
+			}
+		}
+		else
+		{
+			//Enemy AI movement goes here
+			var dir = transform.forward;
+			dir.Normalize ();
+			var z = Mathf.Pow(Mathf.Pow(dir.z, 2)+Mathf.Pow(dir.x, 2), 0.5f);
+			if (z < 0)
+				z = -z;
+			z = z * Time.deltaTime * speed;
+
+			transform.Translate (0, 0, z);
+			if (OutOfBounds())
+			{
+				transform.Translate (0, 0, -z);
+				transform.Rotate (0, 60, 0);
+			}
 		}
 	}
 
@@ -66,6 +95,11 @@ public class EnemyController : NetworkBehaviour {
 
 		// Destroy the bullet after 2 seconds
 		Destroy(bullet, 2.0f);
+	}
+
+	public void StopWaiting()
+	{
+		waiting = false;
 	}
 
 	void OnTriggerEnter(Collider other)
