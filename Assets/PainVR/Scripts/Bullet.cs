2ad4dfe2a657be656d5c260dc9cdfc197ff48af3
Haiwei Ma <maxxx979@CS-evanston.local>
2018-05-04 17:28:57 -0500

    add the hit sounds

--- Assets/PainVR/Scripts/Bullet.cs
+++ Assets/PainVR/Scripts/Bullet.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 public class Bullet : MonoBehaviour {
 
 	public GameObject shooter;
+	//public AudioClip crashSoft;
+	//private AudioSource source;
 
 	void Start()
 	{
 		shooter = gameObject.transform.parent.gameObject;
 		gameObject.transform.parent = null;
+		//source = GetComponent<AudioSource> ();
 	}
 
 	void OnCollisionEnter(Collision collision)
@@ -19,6 +22,8 @@ public class Bullet : MonoBehaviour {
 
 		if (health != null)
 		{
+			//Debug.Log ("hit");
+			//source.PlayOneShot (crashSoft, 0.5F);
 			health.TakeDamage (shooter);
 		}
 
